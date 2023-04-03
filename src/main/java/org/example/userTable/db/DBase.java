8d41db4016a9667b16820ece48ac0aa1396e0b1b
Queenzy-Ongeye <quienzy@food4education.org>
2023-04-03 15:33:18 +0300

    feat:imported jdbc and added Sql2o dependencies

--- src/main/java/org/example/userTable/db/DBase.java
+++ src/main/java/org/example/userTable/db/DBase.java
@@ -1,5 +1,7 @@
 package org.example.userTable.db;
 
+import org.sql2o.Sql2o;
+
 public class DBase {
-//    public static Sql2o sql2o = new S
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/quiz-app", "queen", "akinyi-05");
 }
