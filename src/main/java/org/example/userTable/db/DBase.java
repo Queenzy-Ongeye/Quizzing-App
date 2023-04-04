c202a7a71bc8738861119a8dbda4a70612a8cc14
Queenzy-Ongeye <quienzy@food4education.org>
2023-04-04 16:32:49 +0300

    feat:installed postgresql and added jdbc dependencies

--- src/main/java/org/example/userTable/db/DBase.java
+++ src/main/java/org/example/userTable/db/DBase.java
@@ -3,5 +3,5 @@ package org.example.userTable.db;
 import org.sql2o.Sql2o;
 
 public class DBase {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/quiz-app", "queen", "akinyi-05");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/quiz_app", "postgres", "code001");
 }
